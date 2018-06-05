d27c1b16fb6efd61a8b5a550f36eced053149044
saxgutierrez <diegofdog@gmail.com>
2018-06-05 11:35:17 -0500

    arreglando problema con test, ya solución final

--- src/App.js
+++ src/App.js
@@ -3,30 +3,29 @@ import './App.css';
 
 class App extends Component {
 
-	constructor () {
+	constructor() {
 		super();
-		this.state ={isGoing: false,text_plane:""}
+		
+		this.state ={isGoing: false}
 		this.handleInputChange = this.handleInputChange.bind(this);
 	}
 
 	handleInputChange(event) {
-    	this.setState({isGoing: event.target.checked,
-    		text_plane: event.target.checked?
-    		 "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." : "" 
-    		});
+    	this.setState({isGoing: event.target.checked});
   	}
+  	
 
   render() {
+   const paragraph = <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>;
     return (
       <div className="wrapper">
         <label><input type="checkbox"
           checked={this.state.isGoing}
            onChange={this.handleInputChange}
              /> Mostrar información importante</label>
-        <p>{this.state.text_plane}</p>
+       		 {this.state.isGoing ? paragraph : null}
       </div>
     );
   }
-
 }
 export default App;
